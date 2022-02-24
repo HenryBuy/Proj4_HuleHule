eb50d07d50e7fe660840e05f8c44de5df079b8ec
HenryBuy <503308@student.fontys.nl>
2022-02-24 13:58:43 +0100

    Initial Commit

---
+++ Shared/Proj4_HuleHuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proj4_HuleHuleApp.swift
+//  Shared
+//
+//  Created by Henry Buysschaert on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Proj4_HuleHuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
